c11d2b1acfd776317d172a91222a14f6891c5422
NightRider0098 <39752890+Nightrider0098@users.noreply.github.com>
2020-11-04 19:48:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
-import React, {Component, useState} from "react";
+
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props)
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+    this.state = {
+      cityList =[{ name: 'Goa', country: 'India' }, { name: 'Amsterdam', country: 'Netherlands' }, { name: 'New York', country: 'USA' }, { name: 'Darjeeling', country: 'India' }, { name: 'Tokyo', country: 'Japan' }, { name: 'Lonavala', country: 'India' }]
     }
+  }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+      </div>
+    )
+  }
 }
 
 
